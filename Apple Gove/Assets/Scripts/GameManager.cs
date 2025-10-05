906b4476e3f6042fc3e8464f56d2f15582a9ac10
OPitaWalsh <opita1@walsh.edu>
2025-10-04 22:23:57 -0400

    Update 1
    Recreated empty Unity project in the proper version
    Imported art
    Created prefabs for player, gamemanager, and tilemap
    Began coding player and gamemanager

---
+++ Apple Gove/Assets/Scripts/GameManager.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //variables
+    public static GameManager instance { get; private set; }
+    public int maxHP { get; private set; }
+    public int currHP { get; private set; }
+    public int item { get; private set; }
+    public int weapon1 { get; private set; }
+    public int weapon2 { get; private set; }
+
+    public float deadzone { get; private set; }
+
+    //UI components
+    [SerializeField]private Image healthbar;
+    [SerializeField]private GameObject winScreen;
+    [SerializeField]private GameObject loseScreen;
+    [SerializeField]private GameObject buttonSet;
+
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        maxHP = currHP = 5;
+        item = 0;
+        weapon1 = 1;
+        weapon2 = 0;
+        SyncHUD();
+
+        deadzone = 0.5f;
+
+        /*
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
+        buttonSet.SetActive(false);
+        */
+    }
+
+
+
+    //variable display
+    public void SyncHUD() {
+        //healthbar.fillAmount = currHP / (float)maxHP;
+        //show item
+        //show weapon1
+        //show weapon2
+    }
+
+
+    //variable control
+    public void HPDown(int amount) {
+        currHP -= amount;
+        SyncHUD();
+        if (currHP < 1)
+            LoseLevel();
+    }
+
+    public void HPUp(int amount) {
+        currHP += amount;
+        if (currHP > maxHP)
+            currHP = maxHP;
+        SyncHUD();
+    }
+
+    public void SetItem(int i) {
+        item = i;
+        SyncHUD();
+    }
+
+    public void SetWeapon1(int i) {
+        weapon1 = i;
+        SyncHUD();
+    }
+
+    public void SetWeapon2(int i) {
+        weapon2 = i;
+        SyncHUD();
+    }
+
+    public void SwitchWeapon() {
+        int a = weapon1;
+        int b = weapon2;
+        weapon1 = b;
+        weapon2 = a;
+        SyncHUD();
+    }
+
+
+
+    //win/lose
+    public void WinLevel() {
+        winScreen.SetActive(true);
+        buttonSet.SetActive(true);
+    }
+
+    public void LoseLevel() {
+        loseScreen.SetActive(true);
+        buttonSet.SetActive(true);
+    }
+
+
+    //menu buttons
+    public void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Start();
+    }
+
+    public void QuitToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+        Destroy(gameObject);
+    }
+}
---
+++ Apple Gove/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float deadzone;
+    //InputActions
+    private InputAction moveAct;
+    private InputAction itemAct;
+    private InputAction switchAct;
+
+
+    void Start()
+    {
+        deadzone = GameManager.instance.deadzone;
+        //inputs
+        moveAct = InputSystem.actions.FindAction("Move");
+        itemAct = InputSystem.actions.FindAction("UseItem");
+        switchAct = InputSystem.actions.FindAction("SwitchWeapon");
+        
+    }
+
+    void Update()
+    {
+        bool hasMoved = false;
+
+        //movement
+        if (moveAct.WasPressedThisFrame()) {
+            Vector2 moveValue = moveAct.ReadValue<Vector2>();
+            if (moveValue.x > deadzone) {
+                print("Move right");
+            } else if (moveValue.x < -deadzone) {
+                print("Move left");
+            } else if (moveValue.y > deadzone) {
+                print("Move up");
+            } else if (moveValue.y < -deadzone) {
+                print("Move down");
+            }
+            hasMoved = true;
+        }
+        
+        //use item
+        if (itemAct.WasPressedThisFrame()) {
+            GameManager.instance.SetItem(0);
+            print("Item used");
+        }
+
+        //switch weapon
+        if (switchAct.WasPressedThisFrame()) {
+            GameManager.instance.SwitchWeapon();
+            print("Weapon switched");
+        }
+
+
+        //tell enemies to move
+        if (hasMoved) {
+            print("Has moved");
+        }
+    }
+}
